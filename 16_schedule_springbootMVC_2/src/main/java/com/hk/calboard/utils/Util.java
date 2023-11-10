9f8aae723234fbcb08d0e8abed5e4fa80a8d230d
HKEDU <HKEDU@DESKTOP-IHD16H3>
2023-11-10 13:59:18 +0900

    third commit

--- 16_schedule_springbootMVC_2/src/main/java/com/hk/calboard/utils/Util.java
+++ 16_schedule_springbootMVC_2/src/main/java/com/hk/calboard/utils/Util.java
@@ -2,8 +2,11 @@ package com.hk.calboard.utils;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
+
+import com.hk.calboard.dtos.CalDto;
 
 @Component
 public class Util {
@@ -26,4 +29,47 @@ public class Util {
 		sdf.format(tm); // 문자열 타입일 경우 date 타입으로 변환해서 사용해야 한다.
 		return sdf.format(tm);
 	}
+	
+	// 요일별 날짜 색깔 적용하기 : 파라미너 -i, dayOfWeek 필요
+	// (공백수 + 현재일) % 7 == 0 토요일
+	// (공백수 + 현재일) % 7 == 1 일요일
+	public static String fontColor(int i, int dayOfWeek) {
+		String str = "black"; // 평일
+		if((dayOfWeek-1+i)%7==0) { // 토요일
+			str = "blue";
+		}else if((dayOfWeek-1+i)%7==1) {
+			str = "red";
+		}
+		return str;
+	}
+	
+	//일일별 일정 목록 구하는 기능
+	public static String getCalViewList(int i, List<CalDto> clist) {
+		String d = isTwo(i+""); // 1 --> "01" 2자리로 변환
+		String calList=""; // "<p>title</p><p>title</p>s<p>title</p>"
+		for (int j = 0; j < clist.size(); j++) {
+			if(clist.get(j).getMdate().substring(6, 8).equals(d)) {
+				calList += "<p>" +( clist.get(j).getTitle().length()>7?
+						clist.get(j).getTitle().substring(0, 7)+"..":
+						clist.get(j).getTitle())+"</p>";
+			}
+		}
+		return calList;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
